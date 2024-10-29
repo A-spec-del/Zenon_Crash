089ab93ae81710e45c9d82c92f3083f5ccfa4f88
A-spec-del <osabiyaemmanuel3@gmail.com>
2024-10-29 23:47:13 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348165846414"
+global.ownernomer = "2348120055226"
 global.ownername = "𝑻𝒐𝒙𝒙𝒊𝒄 𝑩𝒐𝒚"
 global.ytname = "YT: ToxxicTech"
 global.socialm = "GitHub: Toxic1239"
